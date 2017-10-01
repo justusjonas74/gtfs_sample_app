68f70e0ea1449d7d3775696fe3f6ae31cac18bfd
justusjonas74 <francis.doege@web.de>
2017-10-01 14:01:10 +0200

    Added SSL

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,18 +5,36 @@ set :application, 'fahrplan'
 set :repo_url, 'git@github.com:justusjonas74/gtfs_sample_app.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :user, 'deploy'
-server '46.101.202.6', user: "#{fetch(:user)}", roles: %w{app db web}, primary: true
+server 'app.francisdoege.com', user: "#{fetch(:user)}", roles: %w{app db web}, primary: true
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :pty, true
 
 set :rvm_ruby_version, '2.4.1'
 
 append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/puma.rb'
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
+append :linked_dirs, 'log', 'public/.well-known', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
+
+
 
 set :config_example_suffix, '.example'
 set :config_files, %w{config/database.yml config/secrets.yml}
 set :puma_conf, "#{shared_path}/config/puma.rb"
+
+set :nginx_domains, "app.francisdoege.com www.app.francisdoege.com"
+set :nginx_use_ssl, false
+set :nginx_ssl_certificate, 'app.francisdoege.crt'
+# SSL certificate file path
+# default value: "/etc/ssl/certs"
+#set :nginx_ssl_certificate_path, "#{shared_path}/ssl/certs"
+
+# Name of SSL certificate private key
+# default value: "#{application}.key"
+set :nginx_ssl_certificate_key, 'app.francisdoege.key'
+
+# SSL certificate private key path
+# default value: "/etc/ssl/private"
+#set :nginx_ssl_certificate_key_path, "#{shared_path}/ssl/private"
+
 
 namespace :deploy do
   before 'check:linked_files', 'config:push'
